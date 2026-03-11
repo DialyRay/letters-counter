a375150324e130589170320bc28118e4ea4a53b0
dialyRay <dialyray@gmail.com>
2026-03-11 21:50:14 +0300

    first

---
+++ letters counter.js
@@ -0,0 +1,16 @@
+function counter(text) {
+    text = text.split( )
+    let count = 0;
+    for (let i = 0; i < text.length; i++) {
+        if(!obj[i]) {
+            obj[i] = 1
+
+        }
+        else {
+            obj[i] = obj[1] + 1;
+        }
+    }
+    return obj[a];
+
+}
+counter('aaabbb'); 
